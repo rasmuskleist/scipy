1f2e55437534f9977639007a4d07530c04e42e57
rasmuskleist <kleist.rasmus@gmail.com>
2024-05-18 09:39:11 +0200

    Add python hodlr boiler plate

---
+++ scipy/hierarchical/_hodlr.py
@@ -0,0 +1,11 @@
+__all__ = ['hodlr']
+
+class hodlr:
+    _format = 'hodlr'
+
+    def __init__(self, a11, a12, a21, a22, d):
+        self.a11 = a11
+        self.a12 = a12
+        self.a21 = a21
+        self.a22 = a22
+        self.d = d
