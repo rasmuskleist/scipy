ffe789ac2bf99fe3390cfa79faeba1952e6ad691
rasmuskleist <kleist.rasmus@gmail.com>
2024-05-19 14:10:35 +0200

    Add info to handle errors

--- scipy/hierarchical/egrss/_basic.py
+++ scipy/hierarchical/egrss/_basic.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .egrss import get_egrss_func
+from scipy.linalg import LinAlgError
 
 def solve_triangular(u, vh, d, b):
     """
@@ -17,8 +18,15 @@ def solve_triangular(u, vh, d, b):
     trsv = get_egrss_func('trsv', (u, vh, d, b))
 
     x = np.copy(b, order='F')
-    # Solve the triangular system
-    trsv("L", "N", u, vh, d, x)
 
-    return x
+    # Solve the triangular system
+    info = trsv("L", "N", u, vh, d, x)
+
+    if info == 0:
+        return x
+    if info > 0:
+        raise LinAlgError("singular matrix: resolution failed at diagonal %d" %
+                          (info-1))
+    raise ValueError('illegal value in %dth argument of internal trtrs' %
+                     (-info))
 
--- scipy/hierarchical/egrss/src/dtrsv.f
+++ scipy/hierarchical/egrss/src/dtrsv.f
@@ -1,11 +1,11 @@
-      SUBROUTINE DTRSV(UPLO,TRANS,N,P,U,VT,D,X)
+      SUBROUTINE DTRSV(UPLO,TRANS,N,P,U,VT,D,B,INFO)
 *
 *     .. Scalar Arguments ..
-      INTEGER N,P
+      INTEGER INFO,N,P
       CHARACTER TRANS,UPLO
 *     ..
 *     .. Array Arguments ..
-      DOUBLE PRECISION D(N), U(N,P), VT(P,N), X(N)
+      DOUBLE PRECISION D(N), U(N,P), VT(P,N), B(N)
 *     ..
 *
 *  =====================================================================
@@ -17,28 +17,29 @@
 *     Start the operations. In this version the elements of A are
 *     accessed sequentially with one pass through A.
 *
+      INFO = 0
       IF (TRANS == 'N') THEN
           IF (UPLO == 'U') THEN
               DO J = N, 1, -1
-                  X(J) = (X(J) - DOT_PRODUCT(Z, U(J,:))) / D(J)
-                  Z = Z + VT(:,J) * X(J)
+                  B(J) = (B(J) - DOT_PRODUCT(Z, U(J,:))) / D(J)
+                  Z = Z + VT(:,J) * B(J)
               ENDDO
           ELSE
               DO J = 1, N
-                  X(J) = (X(J) - DOT_PRODUCT(Z, U(J,:))) / D(J)
-                  Z = Z + VT(:,J) * X(J)
+                  B(J) = (B(J) - DOT_PRODUCT(Z, U(J,:))) / D(J)
+                  Z = Z + VT(:,J) * B(J)
               ENDDO
           END IF
       ELSE
           IF (UPLO == 'U') THEN
               DO J = N, 1, -1
-                  X(J) = (X(J) - DOT_PRODUCT(Z, VT(J,:))) / D(J)
-                  Z = Z + U(:,J) * X(J)
+                  B(J) = (B(J) - DOT_PRODUCT(Z, VT(J,:))) / D(J)
+                  Z = Z + U(:,J) * B(J)
               ENDDO
           ELSE
               DO J = 1, N
-                  X(J) = (X(J) - DOT_PRODUCT(Z, VT(J,:))) / D(J)
-                  Z = Z + U(:,J) * X(J)
+                  B(J) = (B(J) - DOT_PRODUCT(Z, VT(J,:))) / D(J)
+                  Z = Z + U(:,J) * B(J)
               ENDDO
           END IF
       END IF
