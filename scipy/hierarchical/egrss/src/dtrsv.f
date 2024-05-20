0ee37bb54a63000c7ce8da8388b1dc1e5c838b1f
rasmuskleist <kleist.rasmus@gmail.com>
2024-05-20 13:34:26 +0200

    Add comments

--- scipy/hierarchical/egrss/src/dtrsv.f
+++ scipy/hierarchical/egrss/src/dtrsv.f
@@ -1,3 +1,140 @@
+*> \brief \b DTRSV
+*
+*  =========== DOCUMENTATION ===========
+*
+*  Definition:
+*  ===========
+*
+*       SUBROUTINE DTRSV(UPLO,TRANS,N,P,U,LDU,VT,LDVT,D,INCD,X,INCX,WORK,LWORK)
+*
+*       .. Scalar Arguments ..
+*       INTEGER INCD,INCX,LDU,LDVT,N
+*       CHARACTER DIAG,TRANS,UPLO
+*       ..
+*       .. Array Arguments ..
+*       DOUBLE PRECISION D(*),U(LDU,*),VT(LDVT,*),X(*)
+*       ..
+*
+*
+*> \par Purpose:
+*  =============
+*>
+*> \verbatim
+*>
+*> DTRSV  solves one of the systems of equations
+*>
+*>    A*x = b,   or   A**T*x = b,
+*>
+*> where b and x are n element vectors and A is an n by n unit, or
+*> non-unit, upper or lower triangular matrix.
+*>
+*> No test for singularity or near-singularity is included in this
+*> routine. Such tests must be performed before calling this routine.
+*> \endverbatim
+*
+*  Arguments:
+*  ==========
+*
+*> \param[in] UPLO
+*> \verbatim
+*>          UPLO is CHARACTER*1
+*>           On entry, UPLO specifies whether the matrix is an upper or
+*>           lower triangular matrix as follows:
+*>
+*>              UPLO = 'U' or 'u'   A is an upper triangular matrix.
+*>
+*>              UPLO = 'L' or 'l'   A is a lower triangular matrix.
+*> \endverbatim
+*>
+*> \param[in] TRANS
+*> \verbatim
+*>          TRANS is CHARACTER*1
+*>           On entry, TRANS specifies the equations to be solved as
+*>           follows:
+*>
+*>              TRANS = 'N' or 'n'   A*x = b.
+*>
+*>              TRANS = 'T' or 't'   A**T*x = b.
+*>
+*>              TRANS = 'C' or 'c'   A**T*x = b.
+*> \endverbatim
+*>
+*> \param[in] DIAG
+*> \verbatim
+*>          DIAG is CHARACTER*1
+*>           On entry, DIAG specifies whether or not A is unit
+*>           triangular as follows:
+*>
+*>              DIAG = 'U' or 'u'   A is assumed to be unit triangular.
+*>
+*>              DIAG = 'N' or 'n'   A is not assumed to be unit
+*>                                  triangular.
+*> \endverbatim
+*>
+*> \param[in] N
+*> \verbatim
+*>          N is INTEGER
+*>           On entry, N specifies the order of the matrix A.
+*>           N must be at least zero.
+*> \endverbatim
+*>
+*> \param[in] A
+*> \verbatim
+*>          A is DOUBLE PRECISION array, dimension ( LDA, N )
+*>           Before entry with  UPLO = 'U' or 'u', the leading n by n
+*>           upper triangular part of the array A must contain the upper
+*>           triangular matrix and the strictly lower triangular part of
+*>           A is not referenced.
+*>           Before entry with UPLO = 'L' or 'l', the leading n by n
+*>           lower triangular part of the array A must contain the lower
+*>           triangular matrix and the strictly upper triangular part of
+*>           A is not referenced.
+*>           Note that when  DIAG = 'U' or 'u', the diagonal elements of
+*>           A are not referenced either, but are assumed to be unity.
+*> \endverbatim
+*>
+*> \param[in] LDA
+*> \verbatim
+*>          LDA is INTEGER
+*>           On entry, LDA specifies the first dimension of A as declared
+*>           in the calling (sub) program. LDA must be at least
+*>           max( 1, n ).
+*> \endverbatim
+*>
+*> \param[in,out] X
+*> \verbatim
+*>          X is DOUBLE PRECISION array, dimension at least
+*>           ( 1 + ( n - 1 )*abs( INCX ) ).
+*>           Before entry, the incremented array X must contain the n
+*>           element right-hand side vector b. On exit, X is overwritten
+*>           with the solution vector x.
+*> \endverbatim
+*>
+*> \param[in] INCX
+*> \verbatim
+*>          INCX is INTEGER
+*>           On entry, INCX specifies the increment for the elements of
+*>           X. INCX must not be zero.
+*>
+*>  Level 2 Blas routine.
+*>
+*>  -- Written on 22-October-1986.
+*>     Jack Dongarra, Argonne National Lab.
+*>     Jeremy Du Croz, Nag Central Office.
+*>     Sven Hammarling, Nag Central Office.
+*>     Richard Hanson, Sandia National Labs.
+*> \endverbatim
+*
+*  Authors:
+*  ========
+*
+*> \author Rasmus Kleist Hørlyck Sørensen
+*
+*> \date May 2024
+*
+*> \ingroup double_blas_level1
+*
+*  =====================================================================
       SUBROUTINE DTRSV(UPLO,TRANS,N,P,U,LDU,VT,LDVT,D,INCD,B,INCB,WORK,
      + LWORK,INFO)
 *
@@ -10,6 +147,9 @@
 *     ..
 *
 *  =====================================================================
+*
+*     .. Local Scalars ..
+      INTEGER I,J,JB,JD,KB,KD
 *     ..
 *     .. External Functions ..
       LOGICAL LSAME
@@ -19,7 +159,9 @@
       EXTERNAL DCOPY
       EXTERNAL XERBLA
 *     ..
-
+*     .. Intrinsic Functions ..
+      INTRINSIC MAX
+*     ..
 *     Test the input parameters.
 *
       INFO = 0
@@ -46,55 +188,90 @@
 *     Quick return if possible.
 *
       IF (N.EQ.0 .OR. P.EQ.0) RETURN
-
+*
+*     Set up the start point in B if the increment is not unity. This
+*     will be  ( N - 1 ) * INCB   too small for descending loops.
+*
+      IF (INCB.LE.0) THEN
+        KB = 1 - (N-1)*INCB
+      ELSE IF (INCX.NE.1) THEN
+        KB = 1
+      END IF
+*
+*     Set up the start point in D if the increment is not unity. This
+*     will be  ( N - 1 ) * INCD  too small for descending loops.
+*
+      IF (INCD.LE.0) THEN
+        KD = 1 - (N-1)*INCD
+      ELSE IF (INCX.NE.1) THEN
+        KD = 1
+      END IF
 *
 *     Start the operations. In this version the elements of A are
 *     accessed sequentially with one pass through A.
 *
       CALL DCOPY(P, 0.0D0, 0, WORK, 1)
       IF (LSAME(TRANS, 'N')) THEN
+*
+*        Form  x := inv( A )*x.
+*
           IF (LSAME(UPLO, 'U')) THEN
-              DO J = N, 1, -1
-                  DO I = 1, P
-                      B(J) = B(J) - U(J, I) * WORK(I)
+              IF (INCB.EQ.1 .AND. INCD.EQ.1) THEN
+                  DO J = N, 1, -1
+                      DO I = 1, P
+                          B(J) = B(J) - U(J, I) * WORK(I)
+                      ENDDO
+                      B(J) = B(J) / D(J)
+                      DO I = 1, P
+                          WORK(I) = WORK(I) + B(J) * VT(I, J)
+                      ENDDO
                   ENDDO
-                  B(J) = B(J) / D(J)
-                  DO I = 1, P
-                      WORK(I) = WORK(I) + B(J) * VT(I, J)
-                  ENDDO
-              ENDDO
+              ELSE
+              END IF
           ELSE
-              DO J = 1, N
-                  DO I = 1, P
-                      B(J) = B(J) - U(J, I) * WORK(I)
+              IF (INCB.EQ.1 .AND. INCD.EQ.1) THEN
+                  DO J = 1, N
+                      DO I = 1, P
+                          B(J) = B(J) - U(J, I) * WORK(I)
+                      ENDDO
+                      B(J) = B(J) / D(J)
+                      DO I = 1, P
+                          WORK(I) = WORK(I) + B(J) * VT(I, J)
+                      ENDDO
                   ENDDO
-                  B(J) = B(J) / D(J)
-                  DO I = 1, P
-                      WORK(I) = WORK(I) + B(J) * VT(I, J)
-                  ENDDO
-              ENDDO
+              ELSE
+              END IF
           END IF
       ELSE
+*
+*        Form  x := inv( A**T )*x.
+*
           IF (LSAME(UPLO, 'U')) THEN
-              DO J = 1, N
-                  DO I = 1, P
-                      B(J) = B(J) - VT(I, J) * WORK(I)
+              IF (INCB.EQ.1 .AND. INCD.EQ.1) THEN
+                  DO J = 1, N
+                      DO I = 1, P
+                          B(J) = B(J) - VT(I, J) * WORK(I)
+                      ENDDO
+                      B(J) = B(J) / D(J)
+                      DO I = 1, P
+                          WORK(I) = WORK(I) + B(J) * U(J, I)
+                      ENDDO
                   ENDDO
-                  B(J) = B(J) / D(J)
-                  DO I = 1, P
-                      WORK(I) = WORK(I) + B(J) * U(J, I)
-                  ENDDO
-              ENDDO
+              ELSE
+              END IF
           ELSE
-              DO J = N, 1, -1
-                  DO I = 1, P
-                      B(J) = B(J) - VT(I, J) * WORK(I)
+              IF (INCB.EQ.1 .AND. INCD.EQ.1) THEN
+                  DO J = N, 1, -1
+                      DO I = 1, P
+                          B(J) = B(J) - VT(I, J) * WORK(I)
+                      ENDDO
+                      B(J) = B(J) / D(J)
+                      DO I = 1, P
+                          WORK(I) = WORK(I) + B(J) * U(J, I)
+                      ENDDO
                   ENDDO
-                  B(J) = B(J) / D(J)
-                  DO I = 1, P
-                      WORK(I) = WORK(I) + B(J) * U(J, I)
-                  ENDDO
-              ENDDO
+              ELSE
+              END IF
           END IF
       END IF
 *
