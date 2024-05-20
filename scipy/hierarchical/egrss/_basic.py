0b34551564aff6189fdbb19ab8224b3f6265cceb
rasmuskleist <kleist.rasmus@gmail.com>
2024-05-20 17:46:33 +0200

    Use dtrsm instead in solve_triangular

--- scipy/hierarchical/egrss/_basic.py
+++ scipy/hierarchical/egrss/_basic.py
@@ -18,16 +18,9 @@ def solve_triangular(u, vh, d, b, trans=0, lower=False, overwrite_b=False):
     trans = {0: 'N', 1: 'T', 2: 'C'}.get(trans, trans)
 
     # Get the function
-    trsv = get_egrss_func('trsv', (u, vh, d, b))
+    trsm = get_egrss_func('trsm', (u, vh, d, b))
 
     # Solve the triangular system
-    x, info = trsv(uplo, trans, u, vh, d, b, overwrite_b=overwrite_b)
-
-    if info == 0:
-        return x
-    if info > 0:
-        raise LinAlgError("singular matrix: resolution failed at diagonal %d" %
-                          (info-1))
-    raise ValueError('illegal value in %dth argument of internal trtrs' %
-                     (-info))
+    x = trsm(uplo, trans, u, vh, d, b, overwrite_b=overwrite_b)
+    return x
 
